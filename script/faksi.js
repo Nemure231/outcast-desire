45b75b2a1395d629dc865d6c7483266a67daf2d9
Nemure231 <neruhi5321@gmail.com>
2021-11-08 14:31:51 +0700

    Menambahkan logo peringatan

--- script/faksi.js
+++ script/faksi.js
@@ -44,12 +44,25 @@ ready(function () {
     const toggle = document.querySelector('#js-change-theme');
     const body = document.querySelector('body');
 
-    const bioWelrod = document.getElementById('bio-welrod');
+    const kWelrod = document.getElementById('bio-welrod');
     const deskripsiWelrod = document.getElementById('deskripsi-welrod');
 
 
     const menu = document.getElementById('buka-tutup-menu');
     const mobile = document.getElementById('mobile-menu');
+
+
+    var kontenAegis = document.getElementById('konten-aegis');
+    var kontenEcr = document.getElementById('konten-ecr');
+    var kontenGaia = document.getElementById('konten-gaia');
+
+    var ikonAegis = document.getElementById('ikon-aegis');
+    var ikonGaia = document.getElementById('ikon-gaia');
+    var ikonEcr = document.getElementById('ikon-ecr');
+
+    var ikonAegisKecil = document.getElementById('ikon-aegis-kecil');
+    var ikonGaiaKecil = document.getElementById('ikon-gaia-kecil');
+    var ikonEcrKecil = document.getElementById('ikon-ecr-kecil');
 
 
     toggle.addEventListener('click', () => {
@@ -59,10 +72,19 @@ ready(function () {
             body.classList.add('text-gray-100');
 
 
-            bioWelrod.classList.remove('bg-gray-100');
-            bioWelrod.classList.add('bg-gray-900');
-            deskripsiWelrod.classList.remove('bg-gray-100');
-            deskripsiWelrod.classList.add('bg-gray-900');
+            kontenAegis.classList.remove('bg-gray-100');
+            kontenAegis.classList.add('bg-gray-900');
+            kontenEcr.classList.remove('bg-gray-100');
+            kontenEcr.classList.add('bg-gray-900');
+            kontenGaia.classList.remove('bg-gray-100');
+            kontenGaia.classList.add('bg-gray-900');
+            ikonAegis.classList.add('text-green-500');
+            ikonEcr.classList.add('text-green-500');
+            ikonGaia.classList.add('text-green-500');
+            ikonAegisKecil.classList.add('text-green-500');
+            ikonEcrKecil.classList.add('text-green-500');
+            ikonGaiaKecil.classList.add('text-green-500');
+
 
 
         } else {
@@ -70,10 +92,19 @@ ready(function () {
             body.classList.remove('text-gray-100');
             body.classList.add('text-gray-900');
 
-            bioWelrod.classList.add('bg-gray-100');
-            bioWelrod.classList.remove('bg-gray-900');
-            deskripsiWelrod.classList.add('bg-gray-100');
-            deskripsiWelrod.classList.remove('bg-gray-900');
+            kontenAegis.classList.add('bg-gray-100');
+            kontenAegis.classList.remove('bg-gray-900');
+            kontenEcr.classList.add('bg-gray-100');
+            kontenEcr.classList.remove('bg-gray-900');
+            kontenGaia.classList.add('bg-gray-100');
+            kontenGaia.classList.remove('bg-gray-900');
+
+            ikonAegis.classList.remove('text-green-500');
+            ikonEcr.classList.remove('text-green-500');
+            ikonGaia.classList.remove('text-green-500');
+            ikonAegisKecil.classList.remove('text-green-500');
+            ikonEcrKecil.classList.remove('text-green-500');
+            ikonGaiaKecil.classList.remove('text-green-500');
 
 
         }
@@ -91,11 +122,11 @@ ready(function () {
 
     tombolTabMarkasAegis.addEventListener('click', () => {
 
-        tombolTabRelasiAegis.classList.remove('rounded-t', 'border-l', 'border-t', 'border-r', 'text-blue-700');
-        tombolTabRelasiAegis.classList.add('text-blue-500', 'hover:text-blue-800');
+        tombolTabRelasiAegis.classList.remove('rounded-t', 'border-l', 'border-t', 'border-r', 'text-green-700');
+        tombolTabRelasiAegis.classList.add('text-green-500', 'hover:text-green-800');
 
-        tombolTabMarkasAegis.classList.remove('text-blue-500', 'hover:text-blue-800');
-        tombolTabMarkasAegis.classList.add('rounded-t', 'border-l', 'border-t', 'border-r', 'text-blue-700');
+        tombolTabMarkasAegis.classList.remove('text-green-500', 'hover:text-green-800');
+        tombolTabMarkasAegis.classList.add('rounded-t', 'border-l', 'border-t', 'border-r', 'text-green-700');
 
         tabMarkasAegis.classList.remove('mr-1');
         tabMarkasAegis.classList.add('-mb-px', 'mr-1');
@@ -107,11 +138,11 @@ ready(function () {
 
     tombolTabRelasiAegis.addEventListener('click', () => {
 
-        tombolTabRelasiAegis.classList.add('rounded-t', 'border-l', 'border-t', 'border-r', 'text-blue-700');
-        tombolTabRelasiAegis.classList.remove('text-blue-500', 'hover:text-blue-800');
+        tombolTabRelasiAegis.classList.add('rounded-t', 'border-l', 'border-t', 'border-r', 'text-green-700');
+        tombolTabRelasiAegis.classList.remove('text-green-500', 'hover:text-green-800');
 
-        tombolTabMarkasAegis.classList.add('text-blue-500', 'hover:text-blue-800');
-        tombolTabMarkasAegis.classList.remove('rounded-t', 'border-l', 'border-t', 'border-r', 'text-blue-700');
+        tombolTabMarkasAegis.classList.add('text-green-500', 'hover:text-green-800');
+        tombolTabMarkasAegis.classList.remove('rounded-t', 'border-l', 'border-t', 'border-r', 'text-green-700');
 
         tabRelasiAegis.classList.remove('mr-1');
         tabRelasiAegis.classList.add('-mb-px', 'mr-1');
@@ -131,13 +162,10 @@ ready(function () {
     });
 
 
-    var kontenAegis = document.getElementById('konten-aegis');
-    var kontenEcr = document.getElementById('konten-ecr');
-    var kontenGaia = document.getElementById('konten-gaia');
 
-    var tombolAegis = document.getElementById('tombol-aegis');
-    var tombolEcr = document.getElementById('tombol-ecr');
-    var tombolGaia = document.getElementById('tombol-gaia');
+    // var tombolAegis = document.getElementById('tombol-aegis');
+    // var tombolEcr = document.getElementById('tombol-ecr');
+    // var tombolGaia = document.getElementById('tombol-gaia');
 
     const klikFaksi = document.getElementsByClassName('klik-faksi');
 
